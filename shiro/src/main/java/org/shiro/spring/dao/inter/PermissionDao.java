799a706d069e0cf48698b3bdbb451ca96099eed0
tom641220480 <tom.zhang@credan.com>
2016-06-28 22:15:46 +0800

    project

--- shiro/src/main/java/org/shiro/spring/dao/inter/PermissionDao.java
+++
@@ -1,9 +0,0 @@
-package org.shiro.spring.dao.inter;
-
-import org.shiro.spring.entity.Permission;
-
-public interface PermissionDao {
-	public Permission createPermission(Permission permission);
-
-    public void deletePermission(Long permissionId);
-}
--- shiro/src/main/java/org/shiro/spring/service/inter/PermissionService.java
+++
@@ -1,9 +0,0 @@
-package org.shiro.spring.service.inter;
-
-import org.shiro.spring.entity.Permission;
-
-
-public interface PermissionService {
-	public Permission createPermission(Permission permission);
-	public void deletePermission(Long permissionId);
-}
