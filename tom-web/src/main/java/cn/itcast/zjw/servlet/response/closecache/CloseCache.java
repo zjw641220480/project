2e232972eb76f811b119d1d559502e2617c906da
zjw641220480 <1967580689@qq.com>
2016-07-09 16:13:46 +0800

    project

--- tom-web/src/main/java/cn/itcast/zjw/servlet/response/closecache/CloseCache.java
+++ tom-web/src/main/java/cn/itcast/zjw/servlet/response/closecache/CloseCache.java
@@ -15,8 +15,6 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class CloseCache extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
     public CloseCache() {
         super();
     }
