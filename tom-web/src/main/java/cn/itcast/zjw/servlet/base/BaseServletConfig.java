eeeafdfebb406d90db9939253acebf1943c9ed80
zjw641220480 <1967580689@qq.com>
2016-07-12 16:24:59 +0800

    project

--- tom-web/src/main/java/cn/itcast/zjw/servlet/base/BaseServletConfig.java
+++ tom-web/src/main/java/cn/itcast/zjw/servlet/base/BaseServletConfig.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.PropertyConfigurator;
-
 /**
  * 
  * @ClassName:BaseServletConfig
