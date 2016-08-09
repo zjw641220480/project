dd4046bde09ce2712f5840a7f48c20cb10f775f0
zhangjunwei <1967580689@qq.com>
2016-08-09 23:56:56 +0800

    project

--- tom-javase/src/main/java/cn/itcast/zjw/socket/tcp/base/TcpClientCircle.java
+++ tom-javase/src/main/java/cn/itcast/zjw/socket/tcp/base/TcpClientCircle.java
@@ -9,12 +9,14 @@ public class TcpClientCircle {
 	public static void main(String[] args) throws Exception{
 		//1:创建客户端的Socket服务,指定目的主机和端口;
 		Socket socket = new Socket("127.0.0.1", 10000);
+		//2:得到TCP中用来写入数据的流;
 		OutputStream outputStream = socket.getOutputStream();
+		String line = null;
 		while(true){
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-			String line = null;
+			line = bufferedReader.readLine();
+			//3:把数据写入到流中,这里没有使用到缓冲的相关内容,所以可以不用刷新即可;
 			outputStream.write(line.getBytes());
-			bufferedReader.close();
 		}
 	}
 }
--- tom-javase/src/main/java/cn/itcast/zjw/socket/tcp/base/TcpServerCircle.java
+++ tom-javase/src/main/java/cn/itcast/zjw/socket/tcp/base/TcpServerCircle.java
@@ -1,22 +1,22 @@
 package cn.itcast.zjw.socket.tcp.base;
 
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TcpServerCircle {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
+		//1:使用ServerSocket来创建服务器接收端
 		ServerSocket serverSocket = new ServerSocket(10000);
-		while(true){
-			Socket socket = serverSocket.accept();
-			System.out.println("客户端IP:\t"+socket.getInetAddress().getHostAddress());
+		//2:获得侦听到的Socket客户端服务
+		Socket socket = serverSocket.accept();
+		System.out.println("客户端IP:\t" + socket.getInetAddress().getHostAddress());
+		while (true) {
+			//3:从Socket中获得读取流,从而获取数据
 			InputStream inputStream = socket.getInputStream();
 			byte[] bytes = new byte[1024];
 			int length = inputStream.read(bytes);
-			while(true){
-				System.out.println(new String(bytes,0,length));
-			}
+			System.out.println(new String(bytes, 0, length));
 		}
 	}
 }
