31fdb7c0afcde4df4cb0bffb70957e2db599fa5c
zjw641220480 <1967580689@qq.com>
2017-02-02 21:20:36 +0800

    新建SpringMvc

--- tom-jsjquery/src/main/java/cn/itcast/zjw/ajax/servlet/JsAjaxServlet.java
+++ tom-jsjquery/src/main/java/cn/itcast/zjw/ajax/servlet/JsAjaxServlet.java
@@ -1,9 +1,6 @@
 package cn.itcast.zjw.ajax.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.management.PlatformManagedObject;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,23 +9,25 @@ import javax.servlet.http.HttpServletResponse;
 public class JsAjaxServlet extends HttpServlet {
 
 	/** @Fields serialVersionUID: */
-	  	
+
 	private static final long serialVersionUID = 1L;
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
 		String username = request.getParameter("username");
 		response.setContentType("text/html;charset=utf-8");
 		response.setCharacterEncoding("UTF-8");
 		System.out.println("GET请求成功");
-		response.getWriter().write("GET请求成功:\t参数为id=\t"+id+";name="+username);
+		response.getWriter().write("GET请求成功:\t参数为id=\t" + id + ";name=" + username);
 	}
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
 		String username = request.getParameter("username");
 		response.setContentType("text/html;charset=utf-8");
 		response.setCharacterEncoding("UTF-8");
 		System.out.println("POST请求成功");
-		response.getWriter().write("POST请求成功:\t参数为id=\t"+id+";name="+username);
+		response.getWriter().write("POST请求成功:\t参数为id=\t" + id + ";name=" + username);
 	}
 
 }
