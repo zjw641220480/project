9e3dabece8f3d22069f1ccfd6cb435d7f79eac2a
zjw641220480 <1967580689@qq.com>
2017-02-04 14:53:21 +0800

    分之合并后再次解决冲突

--- tom-jsjquery/src/main/java/cn/itcast/zjw/ajax/servlet/JsAjaxServlet.java
+++ tom-jsjquery/src/main/java/cn/itcast/zjw/ajax/servlet/JsAjaxServlet.java
@@ -1,42 +1,7 @@
-<<<<<<< Upstream, based on origin/mybatis_v2
 package cn.itcast.zjw.ajax.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.lang.management.PlatformManagedObject;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public class JsAjaxServlet extends HttpServlet {
-
-	/** @Fields serialVersionUID: */
-	  	
-	private static final long serialVersionUID = 1L;
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
-		String username = request.getParameter("username");
-		response.setContentType("text/html;charset=utf-8");
-		response.setCharacterEncoding("UTF-8");
-		System.out.println("GET请求成功");
-		response.getWriter().write("GET请求成功:\t参数为id=\t"+id+";name="+username);
-	}
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String id = request.getParameter("id");
-		String username = request.getParameter("username");
-		response.setContentType("text/html;charset=utf-8");
-		response.setCharacterEncoding("UTF-8");
-		System.out.println("POST请求成功");
-		response.getWriter().write("POST请求成功:\t参数为id=\t"+id+";name="+username);
-	}
-
-}
-=======
-package cn.itcast.zjw.ajax.servlet;
-
-import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -67,4 +32,3 @@ public class JsAjaxServlet extends HttpServlet {
 	}
 
 }
->>>>>>> 31fdb7c 新建SpringMvc
--- tom_mybatis_v2/src/main/java/cn/itcast/zjw/mybatis/pojo/Items.java
+++ tom_mybatis_v2/src/main/java/cn/itcast/zjw/mybatis/pojo/Items.java
@@ -1,71 +1,3 @@
-<<<<<<< Upstream, based on origin/mybatis_v2
-package cn.itcast.zjw.mybatis.pojo;
-
-import java.util.Date;
-
-public class Items {
-	private Integer id;
-	private String name;
-	private double price;
-	private String detail;
-	private String pic;
-	private Date createtime;
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public String getDetail() {
-		return detail;
-	}
-
-	public void setDetail(String detail) {
-		this.detail = detail;
-	}
-
-	public String getPic() {
-		return pic;
-	}
-
-	public void setPic(String pic) {
-		this.pic = pic;
-	}
-
-	public Date getCreatetime() {
-		return createtime;
-	}
-
-	public void setCreatetime(Date createtime) {
-		this.createtime = createtime;
-	}
-
-	@Override
-	public String toString() {
-		return "Items [id=" + id + ", name=" + name + ", price=" + price + ", detail=" + detail + ", pic=" + pic
-				+ ", createtime=" + createtime + "]";
-	}
-}
-=======
 package cn.itcast.zjw.mybatis.pojo;
 
 import java.util.Date;
@@ -133,4 +65,3 @@ public class Items {
 				+ ", createtime=" + createtime + "]";
 	}
 }
->>>>>>> 8f891cb shanchu springmvc
