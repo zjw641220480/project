e4123d27137061c80a0ac2c8ed79dd46735dd327
zjw641220480 <1967580689@qq.com>
2017-07-31 18:03:54 +0800

    传统的synchronized方式的生产消费模型

---
+++ tom-javase/src/main/java/cn/itcast/zjw/thread/originalcp/Producer.java
@@ -0,0 +1,18 @@
+package cn.itcast.zjw.thread.originalcp;
+
+public class Producer {
+	private Depot depot;
+
+	public Producer(Depot depot) {
+		this.depot = depot;
+	}
+
+	// 消费产品：新建一个线程向仓库中生产产品。
+	public void produce(final int val) {
+		new Thread() {
+			public void run() {
+				depot.produce(val);
+			}
+		}.start();
+	}
+}
---
+++ tom-javase/src/main/java/cn/itcast/zjw/thread/originalcp/thread_node.md
@@ -0,0 +1,4 @@
+# tom-javase-reflect
+java中的thread-->thread;
+最原始的生产者消费者模型;
+	
