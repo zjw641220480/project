4102087e7704d59948e41dbc723ea2cfe060d736
张俊伟 <zhangjunwei@wdb007.com>
2017-08-24 08:45:24 +0800

    增加一个类

---
+++ tom-javase/src/main/java/cn/itcast/zjw/design/decorator/pojo/TangGao.java
@@ -0,0 +1,12 @@
+package cn.itcast.zjw.design.decorator.pojo;
+
+import cn.itcast.zjw.design.decorator.Programmer;
+
+public class TangGao implements Programmer {
+
+	public void programme() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
