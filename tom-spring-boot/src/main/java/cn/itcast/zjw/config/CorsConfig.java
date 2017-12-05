a239aa3e3ad9c11c5690d20a3667f26464f879b0
张俊伟 <zhangjunwei@wdb007.com>
2017-12-05 09:32:22 +0800

    代码提交

---
+++ tom-spring-boot/src/main/java/cn/itcast/zjw/config/CorsConfig.java
@@ -0,0 +1,31 @@
+package cn.itcast.zjw.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+/**
+ * 
+ * @ClassName: CorsConfig.java
+ * @Description:解决ajax跨域请求相关
+ * @Time 2017年9月21日
+ * @author: Tom
+ */
+@Configuration
+public class CorsConfig {
+	private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
