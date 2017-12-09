f2ce4ddecde818a30a1cb2dfc2f50b469e7b1dda
张俊伟 <zhangjunwei@wdb007.com>
2017-12-09 17:03:20 +0800

    tom-spring-cloud

---
+++ tom-spring-cloud/src/main/java/cn/itcast/zjw/AppApplication.java
@@ -0,0 +1,30 @@
+package cn.itcast.zjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableEurekaServer
+@SpringBootApplication
+@RestController
+public class AppApplication extends SpringBootServletInitializer{
+	
+	@RequestMapping("/index")
+	public String index() {
+		System.out.println("AppApplication.index()");
+		return "index";
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+}
